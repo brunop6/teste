542192abb9fb99af79bc646753278c85bf8ed002
brunop6 <peresbruno6@gmail.com>
2021-04-29 17:06:54 -0300

    Correção

--- criaBotoes.php
+++ criaBotoes.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="javascript" href="functions.js">
+    <script scr="functions.js"></script>
 </head>
 <body>
     <header>
