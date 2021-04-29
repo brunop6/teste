89e1280c6e25bdf2f35cf028f1c952241ecbb62b
brunop6 <83294546+brunop6@users.noreply.github.com>
2021-04-29 17:18:44 -0300

    Add files via upload

---
+++ Contador/cont.js
@@ -0,0 +1,13 @@
+function decrement() {
+    var i = n.textContent;
+    n.textContent = --i;
+}
+
+function increment() {
+    var i = n.textContent;
+    n.textContent = ++i;
+}
+
+function reset() {
+    n.textContent = 0;
+}
